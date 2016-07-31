51c9a494b1a75a9c46f6c10bb192be614c81b58f
Lisa <misslisazhou@gmail.com>
2016-07-30 20:12:07 -1000

    create seeders for users table

--- seeders/20160729045124-add-photos.js
+++ seeders/20160729045124-add-photos.js
@@ -69,24 +69,6 @@ module.exports = {
   },
 
   down: function (queryInterface, Sequelize) {
-    queryInterface.bulkDelete('photos', [{
-      link: 'http://pixel.nymag.com/imgs/daily/vulture/2015/09/04/04-bb8-2.nocrop.w529.h373.jpg'
-    }, {
-      link: 'http://blog.chegg.com/wp-content/uploads/2012/11/cute-high-five-cat.jpg'
-    }, {
-      link: 'http://www.nataliastar2015.com/wp-content/uploads/2015/08/Cute-Pug.jpeg'
-    }, {
-      link: 'http://c.fastcompany.net/multisite_files/cocreate/imagecache/1280/poster/2012/11/1681874-poster-1920-how-adventure-time-keeps-it-real.jpg'
-    }, {
-      link: 'http://67.media.tumblr.com/30b1b0d0a42bca3759610242a1ff0348/tumblr_nnjxy1GQAA1tpo3v2o1_540.jpg'
-    }, {
-      link: 'http://rdcnewscdn.realtor.com/wp-content/uploads/2015/09/Chompers-with-Book-e1442854033632.jpg'
-    }, {
-      link: 'http://www.pleated-jeans.com/wp-content/uploads/2016/01/1-maru.jpg'
-    }, {
-      link: 'http://www.antarasdiary.com/wp-content/uploads/2011/04/bunny/sweetness.jpg'
-    }, {
-      link: 'http://s3.amazonaws.com/assets.prod.vetstreet.com/a9/4f/85c88515433b975862923869bd3b/EmergencyPuppy.jpg'
-    }])
+    return queryInterface.bulkDelete('photos')
   }
 };
