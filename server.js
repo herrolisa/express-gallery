c6bcc430cb877dd9d044aba2b945915f09689e6a
Lisa <misslisazhou@gmail.com>
2016-07-30 19:12:50 -1000

    create local authentication with Passport

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var express = require('express');
+var session = require('express-session');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var db = require('./models');
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;  // Want to use Basic Authentication Strategy
-
 
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, 'views'));
@@ -16,31 +16,83 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var user = { username: 'bob', password: 'secret', email: 'bob@example.com' };
-passport.use(new BasicStrategy(
-  function(username, password, done) {
-    // Example authentication strategy using
-    if ( !(username === user.username && password === user.password) ) {
-      return done(null, false);
-    }
-    return done(null, user);
-}));
+// var user = { username: 'bob', password: 'secret', email: 'bob@example.com' };
+// passport.use(new BasicStrategy(
+//   function(username, password, done) {
+//     // Example authentication strategy using
+//     if ( !(username === user.username && password === user.password) ) {
+//       return done(null, false);
+//     }
+//     return done(null, user);
+// }));
 
-app.use(methodOverride('_method'));
-app.use(methodOverride(function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method;
-    delete req.body._method;
-    return method;
+// app.use(methodOverride('_method'));
+// app.use(methodOverride(function(req, res){
+//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//     // look in urlencoded POST bodies and delete it
+//     var method = req.body._method;
+//     delete req.body._method;
+//     return method;
+//   }
+// }));
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    console.log(username, password);
+    var USERNAME = CONFIG.SECRETS.USERNAME;
+    var PASSWORD = CONFIG.SECRETS.PASSWORD;
+    if (username === USERNAME && password === PASSWORD){
+      return done(null, {});
+    }
+
+    // User.findOne({ username: username }, function (err, user) {
+    //   if (err) { return done(err); }
+    //   if (!user) {
+    //     return done(null, false, { message: 'Incorrect username.' });
+    //   }
+    //   if (!user.validPassword(password)) {
+    //     return done(null, false, { message: 'Incorrect password.' });
+    //   }
+    //   return done(null, user);
+    // });
   }
-}));
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // GET /index.html
 app.get('/', function(req, res) {
   db.photos.findAll({}).then(function(photosArray) {
     res.render('index', {result: photosArray});
   });
+});
+
+// LOGIN PAGE
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/secret',
+    failureRedirect: '/login'
+    }));
+
+app.get('/secret', function (req, res) {
+  res.render('secret');
 });
 
 // GET /gallery/new (page with form to add new photo to gallery)
