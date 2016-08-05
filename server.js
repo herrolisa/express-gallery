fc7dec5a28a771269caee264eaf522f6aaaa37f2
Lisa <misslisazhou@gmail.com>
2016-08-04 21:28:21 -1000

    authentication works

--- server.js
+++ server.js
@@ -1,82 +1,91 @@
+// REQUIRED MODULES
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var express = require('express');
-var session = require('express-session');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var db = require('./models');
+var CONFIG = require('./config.json');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
+// PASSPORT
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;  // Want to use Basic Authentication Strategy
+var LocalStrategy = require('passport-local').Strategy;
 
+
+// SET UP PUG TEMPLATES
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, 'views'));
 
-app.use(express.static('public'));
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')); // for static image and css files
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({ extended: true })); //get information from html forms; add items to req.body
 
-// var user = { username: 'bob', password: 'secret', email: 'bob@example.com' };
-// passport.use(new BasicStrategy(
-//   function(username, password, done) {
-//     // Example authentication strategy using
-//     if ( !(username === user.username && password === user.password) ) {
-//       return done(null, false);
-//     }
-//     return done(null, user);
-// }));
-
-// app.use(methodOverride('_method'));
-// app.use(methodOverride(function(req, res){
-//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-//     // look in urlencoded POST bodies and delete it
-//     var method = req.body._method;
-//     delete req.body._method;
-//     return method;
-//   }
-// }));
-
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    console.log(username, password);
-    var USERNAME = CONFIG.SECRETS.USERNAME;
-    var PASSWORD = CONFIG.SECRETS.PASSWORD;
-    if (username === USERNAME && password === PASSWORD){
-      return done(null, {});
-    }
-
-    // User.findOne({ username: username }, function (err, user) {
-    //   if (err) { return done(err); }
-    //   if (!user) {
-    //     return done(null, false, { message: 'Incorrect username.' });
-    //   }
-    //   if (!user.validPassword(password)) {
-    //     return done(null, false, { message: 'Incorrect password.' });
-    //   }
-    //   return done(null, user);
-    // });
+// SET UP METHOD-OVERRIDE (to use PUT and DELETE methods in html)
+app.use(methodOverride('_method'));
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
   }
-));
+}));
 
+// SET UP EXPRESS-SESSION
+app.use(session({
+  store: new RedisStore(),
+  secret: CONFIG.secret,
+  resave: true,
+  saveUninitialized: false
+}));
+
+// set up persistent login sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    db.users.findOne({username: username}).then(function(user){
+      if (!user){
+        return done(null, false, { message: 'Nobody here by that name'} );
+      }
+      if (user.password !== password){
+        return done(null, false, { message: 'Wrong password'} );
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  console.log('serialize:', user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log('deserialize:', id);
+  db.users.findOne({id: id}).then(function(user) {
+    done(null, user.id);
+  });
+});
+
+function authenticationMiddleware (req, res, next) {
+  console.log('authentication middleware');
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+// ROUTES ======================================================================
 // GET /index.html
 app.get('/', function(req, res) {
-  db.photos.findAll({}).then(function(photosArray) {
+  console.log(req.session);
+  db.photos.findAll().then(function(photosArray) {
     res.render('index', {result: photosArray});
   });
 });
@@ -87,9 +96,15 @@ app.get('/login', function(req, res) {
 });
 app.post('/login',
   passport.authenticate('local', {
-    successRedirect: '/secret',
+    successRedirect: '/',
     failureRedirect: '/login'
-    }));
+  }));
+
+// LOGOUT
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 app.get('/secret', function (req, res) {
   res.render('secret');
@@ -97,14 +112,16 @@ app.get('/secret', function (req, res) {
 
 // GET /gallery/new (page with form to add new photo to gallery)
 app.get('/gallery/new',
-  passport.authenticate('basic', { session: false }), // Require Login for '/gallery/new'
+  // passport.authenticate('basic', { session: false }), // Require Login for '/gallery/new'
+  authenticationMiddleware,
   function(req, res) {
     res.render('add-form');
   });
 
 // POST to /gallery (from the form of /gallery/new)
 app.post('/gallery',
-  passport.authenticate('basic', { session: false }), // Require Login for POST '/gallery'
+  // passport.authenticate('basic', { session: false }), // Require Login for POST '/gallery'
+  authenticationMiddleware,
   function(req, res) {
     db.photos.create({
       link: req.body.link,
@@ -139,7 +156,8 @@ app.get('/gallery/:id', function(req, res) {
 
 // GET /gallery/[photo id]/edit (page with form to edit current photo)
 app.get('/gallery/:id/edit',
-  passport.authenticate('basic', { session: false }), // Require Login for '/gallery/[photo id]/edit'
+  // passport.authenticate('basic', { session: false }), // Require Login for '/gallery/[photo id]/edit'
+  authenticationMiddleware,
   function (req, res) {
     if (isNaN(Number(req.params.id))){
       res.status(404).render('404');
@@ -160,7 +178,8 @@ app.get('/gallery/:id/edit',
 
 // PUT to /gallery/[photo id]
 app.put('/gallery/:id',
-  passport.authenticate('basic', { session: false }), // Require Login for PUT '/gallery/[photo id]'
+  // passport.authenticate('basic', { session: false }), // Require Login for PUT '/gallery/[photo id]'
+  authenticationMiddleware,
   function(req, res) {
     db.photos.find({
       where: {
@@ -183,7 +202,8 @@ app.put('/gallery/:id',
 
 // DELETE [photo id]
 app.delete('/gallery/:id',
-  passport.authenticate('basic', { session: false }), // Require Login to DELETE /gallery/:id''
+  // passport.authenticate('basic', { session: false }), // Require Login to DELETE /gallery/:id''
+  authenticationMiddleware,
   function(req, res) {
     db.photos.destroy({
       where: {
@@ -198,10 +218,12 @@ app.delete('/gallery/:id',
     });
   });
 
+// catch all to redirect pages that don't exist to 404
 app.use('*', function (err, res, next) {
   res.status(404).render('404');
 });
 
+// LAUNCH ======================================================================
 var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
