b8c6a1b10d2a3eeae87a9dcb785e9640587b9633
Lisa <misslisazhou@gmail.com>
2016-08-06 16:37:03 -1000

    update association

--- models/photos.js
+++ models/photos.js
@@ -7,7 +7,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        models.photos.belongsTo(models.users);
       }
     }
   });
