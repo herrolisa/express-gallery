9c0463b5a177f29ff3bb8aa1fc922cb382067816
Lisa <misslisazhou@gmail.com>
2016-08-10 19:07:48 -1000

    add association for users on photos

--- models/users.js
+++ models/users.js
@@ -6,7 +6,9 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        models.users.hasMany(models.photos, {
+          foreignKey: 'user_id'
+        });
       }
     }
   });
